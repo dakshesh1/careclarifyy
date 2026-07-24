8e8bcccc0c8579963f620fe78b2852ffc295da65
dakshesh1 <62845795+dakshesh1@users.noreply.github.com>
2026-07-24 12:33:34 +0530

    fastapi server for railway

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500","http://127.0.0.1:5500","https://careclarifyy.onrender.com"],
+    allow_origins=["http://localhost:5500","http://127.0.0.1:5500","https://careclarifyy.onrender.com","https://careclarifyy-production.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
