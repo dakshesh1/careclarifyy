482c710f2d6b42da7baf2245c0e3a5bf97f9e2a4
dakshesh1 <daksheshtiwari30@gmail.com>
2026-07-24 05:36:28 +0530

    render host

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5500","http://127.0.0.1:5500","https://statuesque-kheer-89f980.netlify.app"],
+    allow_origins=["http://localhost:5500","http://127.0.0.1:5500","https://careclarifyy.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
